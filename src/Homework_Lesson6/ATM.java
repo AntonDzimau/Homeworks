f51788197dd7d185798a63b4d859f2c9e5dee2d8
AntonDzimau <chehov.anton96@gmail.com>
2022-08-23 20:16:30 +0300

    Improved the additional task of lesson 6.

--- src/Homework_Lesson6/ATM.java
+++ src/Homework_Lesson6/ATM.java
@@ -11,64 +11,75 @@ public class ATM {
         this.count50 = count50;
         this.count100 = count100;
     }
-/**НЕВЕРНО СЧИТАЕТ КОЛИЧЕСТВО КУПЮР*/
+
+    /**
+     * НЕВЕРНО СЧИТАЕТ КОЛИЧЕСТВО КУПЮР
+     */
     public boolean withdrawCash(int withdrawCash) {
         int issued100 = 0;
         int issued50 = 0;
         int issued20 = 0;
-        for (int i = this.totalAmount; this.totalAmount > 0; ) {
+        System.out.println("You want to withdraw a $" + withdrawCash + ". One minute, please.");
+        for (; this.totalAmount > 0; ) {
             if (this.totalAmount < withdrawCash) {
                 System.out.println("Error. Not enough money. The ATM has a $" + this.totalAmount + ". Choice other amount and try again.");
                 break;
             }
-            if (withdrawCash >= 100) {
+            if (withdrawCash >= 100 && count100 >= withdrawCash / 100) {
                 count100 -= withdrawCash / 100;
                 this.totalAmount = this.totalAmount - (withdrawCash / 100) * 100;
+                issued100 += (withdrawCash / 100);
                 withdrawCash = withdrawCash - (withdrawCash / 100) * 100;
-                ++issued100;
-            } else if (withdrawCash >= 50) {
+            } else if (withdrawCash >= 50 && count50 >= withdrawCash / 50) {
                 count50 -= withdrawCash / 50;
                 this.totalAmount = this.totalAmount - (withdrawCash / 50) * 50;
+                issued50 += (withdrawCash / 50);
                 withdrawCash = withdrawCash - (withdrawCash / 50) * 50;
-                ++issued50;
-            } else if (withdrawCash >= 20) {
+            } else if (withdrawCash >= 20 && count20 >= withdrawCash / 20) {
                 count20 -= withdrawCash / 20;
                 this.totalAmount = this.totalAmount - (withdrawCash / 20) * 20;
+                issued20 += (withdrawCash / 20);
                 withdrawCash = withdrawCash - (withdrawCash / 20) * 20;
-                ++issued20;
             } else if (withdrawCash > 0) {
                 System.out.println("Error. ATM has no banknotes to dispense. Please, contact support.");
-                System.out.println("I gave you: " + issued100 + " $100 bills, " + issued50 + " $50 bills and " + issued20 + " 20 bills.");
+                System.out.println("I gave you: " + issued100 + " $100 bills, " + issued50 + " $50 bills and " + issued20 + " $20 bills.");
                 break;
             } else if (withdrawCash == 0) {
-                System.out.println("Give your money, honey. I gave you: " + issued100 + " $100 bills, " + issued50 + " $50 bills and " + issued20 + " 20 bills.");
+                System.out.println("Give your money, honey. I gave you: " + issued100 + " $100 bills, " + issued50 + " $50 bills and " + issued20 + " $20 bills.");
                 break;
             }
         }
-        return true;
+        return true; //я не понимаю, какую роль он играет здесь
     }
 
-    int addCashToATM(int addedCash) {
-        if (addedCash == 20) {
-            totalAmount += 20;
-            count20++;
-        } else if (addedCash == 50) {
-            totalAmount += 50;
-            count50++;
-        } else if (addedCash == 100) {
-            totalAmount += 100;
-            count100++;
-        } else if (addedCash != 100 && addedCash != 50 && addedCash != 20) {
+    int addCashToATM(int addedBill, int numberOfBanknotes) {
+        if (addedBill == 20) {
+            totalAmount += 20 * numberOfBanknotes;
+            count20 += numberOfBanknotes;
+            System.out.println("OK. I accept " + numberOfBanknotes + " banknotes of " + addedBill + " bucks.");
+        } else if (addedBill == 50) {
+            totalAmount += 50 * numberOfBanknotes;
+            count50 += numberOfBanknotes;
+            System.out.println("OK. I accept " + numberOfBanknotes + " banknotes of " + addedBill + " bucks.");
+        } else if (addedBill == 100) {
+            totalAmount += 100 * numberOfBanknotes;
+            count100 += numberOfBanknotes;
+            System.out.println("OK. I accept " + numberOfBanknotes + " banknotes of " + addedBill + " bucks.");
+        } else {
             System.out.println("Insert a 20, 50 or 100 dollars bill.");
         }
         return totalAmount;
     }
 
     void infoAboutATM() {
-        System.out.println("Total amount of money in ATM: " + totalAmount);
-        System.out.println("The number of 20 dollar bills is: " + count20);
-        System.out.println("The number of 50 dollar bills is: " + count50);
-        System.out.println("The number of 100 dollar bills is: " + count100);
+        System.out.println("Total amount of money in ATM: $" + totalAmount);
+        System.out.println("The number of $100 bills is: " + count100);
+        System.out.println("The number of $50 bills is: " + count50);
+        System.out.println("The number of $20 bills is: " + count20);
+    }
+
+    void infoAboutTotalAmountInATM() {
+        System.out.println("Total amount of money in ATM: $" + totalAmount);
     }
 
 }
--- src/Homework_Lesson6/Main.java
+++ src/Homework_Lesson6/Main.java
@@ -3,22 +3,20 @@ package Homework_Lesson6;
 public class Main {
     public static void main(String[] args) {
         System.out.println("\nTHE FIRST ADDITIONAL TASK.\n");
+
         Computer firstComputer = new Computer(800, "ASUS FX505DU");
         firstComputer.infoAboutComputer();
         Computer secondComputer = new Computer(1000, "Panasonic CF-31", new RAM("Kingston", 16), new HDD("Seagate", 512, "external"));
         secondComputer.infoAboutComputer();
+
         System.out.println("\nSECOND ADDITIONAL TASK.\n");
+
         ATM atm = new ATM(0, 0, 0);
-        atm.addCashToATM(20);
-        atm.addCashToATM(100);
-        atm.addCashToATM(100);
-        atm.addCashToATM(100);
-        atm.addCashToATM(100);
-        atm.addCashToATM(100);
-        atm.addCashToATM(50);
-        atm.addCashToATM(20);
+        atm.addCashToATM(100, 5);
+        atm.addCashToATM(50, 1);
+        atm.addCashToATM(10, 1);
         atm.infoAboutATM();
-        atm.withdrawCash(450);
+        atm.withdrawCash(492);
         atm.infoAboutATM();
 
     }
